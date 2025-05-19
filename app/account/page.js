37ca332b812735a2aef7182477fca5b8670c8b79
Sundar Potlacheruvu <sundar.potlacheruvu@advance-auto.com>
2025-05-19 08:33:57 +0530

    commit

---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "Account Page",
+};
+
+const Page = () => {
+  return <div>Page</div>;
+};
+
+export default Page;
---
+++ app/components/Navigation.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/cabins">Cabins</Link>
+      </li>
+      <li>
+        <Link href="/account">Account</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default Navigation;
