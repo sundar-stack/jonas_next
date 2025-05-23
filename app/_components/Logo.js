b2e38a4ad4d8175a6ef3e127f4cf3c419401b55f
Sundar Potlacheruvu <sundar.potlacheruvu@advance-auto.com>
2025-05-23 19:16:32 +0530

    'test'

---
+++ app/_components/Logo.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import LogoURL from '../../public/logo.png'
+
+
+function Logo() {
+  return (
+    <Link href="/" className="flex items-center gap-4 z-10">
+      <Image src={LogoURL} height="60" width="60" alt="The Wild Oasis logo" />
+      <span className="text-xl font-semibold text-primary-100">
+        The Wild Oasis
+      </span>
+    </Link>
+  );
+}
+
+export default Logo;
